a320262be0f0fb96c16ce135989d8dcf426bcb43
raulivangarcia <raulgarciadesign@gmail.com>
2020-09-21 20:14:25 -0600

    mon

--- js/index.js
+++ js/index.js
@@ -1,1 +1,2 @@
-if
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', time: 'numeric'};
+document.getElementById('last-updated').textContent = new Date().toLocaleDateString('en-US', options);
