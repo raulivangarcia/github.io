0cb5180f995b2e6669112890e032f1ef3a23b60f
raulivangarcia <raulgarciadesign@gmail.com>
2020-10-02 16:08:59 -0600

    js

--- js/date.js
+++ js/date.js
@@ -1,2 +1,1 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("last-updated").textContent = new Date().toLocaleDateString('en-US', options);
+let oLastModif = new Date(document.lastModified);
