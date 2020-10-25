6946902568ac1a59bc2622feb9dc8d519e6639c5
raulivangarcia <raulgarciadesign@gmail.com>
2020-10-24 23:57:33 -0600

    sat

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,11 @@
-function windchill() {
+window.onload = function windchill() {
     let t = parseFloat(document.querySelector("#currenttemp").textContent);
     let s = parseFloat(document.querySelector("#windspeed").textContent);
     let output = "N/A";
-    if(t <= 50 && s >= 3) {
-        let f = 35.74 + .6215*t - (35.75 * s ^ .16 + (.4275 * (s ^ .16)));
-        document.getElementById("output").innerHTML
+    if (t <= 50 && s >= 3) {
+        let f = 35.74 + .6215 * t - (35.75 * s ^ .16 + (.4275 * (s ^ .16)));
+        output = Math.round(f);
     }
+        document.getElementById("output").innerHTML
+
 }
