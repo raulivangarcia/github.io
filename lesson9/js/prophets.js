8b3766275032b4bf0f6860d79174c18f2ca139a2
raulivangarcia <raulgarciadesign@gmail.com>
2020-11-12 23:22:13 -0700

    thurs

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -4,15 +4,29 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        //console.table(jsonObject);  // temporary checking for valid response and data parsing
         const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
+        prophets.forEach(prophet => {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let p = document.createElement('p');
+            let bp = document.createElement('p')
+            let image = document.createElement('img');
+
+            //add details and attributes            
+            h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+            p.innerHTML = `Birth Date: ${prophet.birthdate}`;
+            bp.innerHTML = `Birth Place: ${prophet.birthplace}`;
+            image.setAttribute('src', prophet.imageurl);
+            image.setAttribute('alt', `Picture of ${prophet.name} ${prophet.lastname}`);
+            //actually add the content to the container
             
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
             card.appendChild(h2);
+            card.appendChild(p);
+            card.appendChild(bp);
+            card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
-        }});
+            document.querySelector('.cards').appendChild(card);
+        });
+
+    });
