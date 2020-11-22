e16038597fedd43e7ca33cc32b95a52cc88b0c9f
raulivangarcia <raulgarciadesign@gmail.com>
2020-11-21 22:14:04 -0700

    Sat2

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -10,7 +10,21 @@ fetch(apiURL)
   document.getElementById('condition').textContent = jsObject.weather[0].main;
   document.getElementById('humidity').textContent = jsObject.main.humidity;
   document.getElementById('windspeed').textContent = jsObject.wind.speed;
-  //document.getElementById('windchill').innerHTML = windchill();
+
+  
+  let t = parseFloat(document.querySelector("#currenttemp").textContent);
+  let s = parseFloat(document.querySelector("#windspeed").textContent);
+  let output;
+  if (t <= 50 && s >= 3) {
+    let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * (t *(Math.pow(s, .16))));
+    output = Math.round(f);
+}
+else {
+    output = "N/A"
+}
+document.getElementById("windchill").innerHTML = output
+
+
 });
 
 //forecast
@@ -25,29 +39,15 @@ fetch(forecast)
     console.log(forecast);
     let day = 0;
     const week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-
-    
      
     forecast.forEach(x => {
        const d = new Date(x.dt_txt);
        const desc = x.weather[0].description;
        const image = "https://openweathermap.org/img/w/" + x.weather[0].icon + ".png";
       document.getElementById(`dayofweek${day+1}`).textContent = week[d.getDay()];
-      document.getElementById(`forecast${day+1}`).textContent = x.main.temp.toFixed(0);
+      document.getElementById(`forecast${day+1}`).textContent = x.main.temp.toFixed(0) + `°F`;
       document.getElementById(`icon${day+ 1}`).setAttribute('src', image);
       document.getElementById(`icon${day+ 1}`).setAttribute('alt', desc);
       day++;
-
-      
-
-
-
-
-
-      // document.getElementById(`temp${days+ 1}`).innerHTML = `${forecast.main.temp.toFixed(0)}`;
-      //document.getElementById(`day${days+ 1}`).textContent = week[d.getDay()];
-      // document.getElementById(`icon${days+ 1}`).setAttribute('src', imgage);
-      // document.getElementById(`icon${days+ 1}`).setAttribute('alt', desc);
-      // days++;
        }) 
 });
