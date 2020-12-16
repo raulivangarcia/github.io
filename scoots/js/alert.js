4b5220b1df3d97ec9c3c200bea5fa34f3f372ec3
raulivangarcia <raulgarciadesign@gmail.com>
2020-12-16 10:40:59 -0700

    wed2

--- scoots/js/alert.js
+++ scoots/js/alert.js
@@ -1,20 +1,16 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=20.5083&lon=-86.9458&exclude=current,minutely,hourly,daily&appid=ddbc2a683be3543c78927678e9fdfbc1&units=imperial';
+// const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=20.5083&lon=-86.9458&exclude=current,minutely,hourly,daily&appid=ddbc2a683be3543c78927678e9fdfbc1&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=64.8378&lon=-147.716&exclude=current,minutely,hourly,daily&appid=ddbc2a683be3543c78927678e9fdfbc1&units=imperial';
+
+
 
 fetch(apiURL)
-.then((response) => response.json())
-.then((jsObject) => {
-  console.log(jsObject);
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 
-  if (jsObject.alerts && jsObject.alerts.length >0) {
-    document.getElementById('message').textContent = jsObject.alerts[0].description;
-    
-    
-  }  
+    if (jsObject.alerts && jsObject.alerts.length > 0) {
+      document.getElementById('message').textContent = jsObject.alerts[0].description;
+      
+    }
   
 });
-// function myFunction() {
-  //     var btn = document.createElement("button");
-  //     btn.innerHTML = "CLOSE";
-  //     document.body.appendChild(btn);
-  //   } 
-  
--- scoots/js/remove.js
+++ scoots/js/remove.js
@@ -1,4 +1,8 @@
 function weathergone() {
 var off = document.getElementById('message');
 off.remove();
+
+}
+function buttongone() {
+    var gone = document.getElementById("close").style.display="none";
 }
